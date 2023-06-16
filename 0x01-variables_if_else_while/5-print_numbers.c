b6b3520151dadaee8124fa3796022644e751e8c7
kiyuenter <kiyuenterprise@gmail.com>
2023-06-16 06:45:06 -0700

    Print numbers upto 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -> prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
