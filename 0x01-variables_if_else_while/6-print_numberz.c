d0bd2765194566a3326b852ff7a80efb3bf97c80
kiyuenter <kiyuenterprise@gmail.com>
2023-06-16 06:50:25 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -> prints the alphabet in lowercase, followed by a new line.
+ *  Return: Always 0 (sucess)
+ */
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
