97523fd4c2e2dead2658486a8d50a5f898819174
kiyuenter <kiyuenterprise@gmail.com>
2023-06-20 07:07:35 -0700

    Print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  * print_alphabet_x10 - function to print abc 10 times*
- *   * Return: 0
- *    */
+ * print_alphabet_x10 - function to print abc 10 times*
+ * Return: 0
+ */
 void print_alphabet_x10(void)
 {
 	char c, i;
