229a6183bb85eece5012ed9ed64fb9fd2a2fb746
kiyuenter <kiyuenterprise@gmail.com>
2023-06-29 07:13:02 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,21 +11,21 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	
+
 	i = 0;
 	j = 0;
-	
+
 	while (dest[i] != '\0')
 		i++;
-	
+
 	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	
+
 	dest[i] = '\0';
-	
+
 	return (dest);
 }
