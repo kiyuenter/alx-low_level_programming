e968d817e75595b16e308f50a33053040ab6b376
kiyuenter <kiyuenterprise@gmail.com>
2023-07-05 18:17:13 -0700

    prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,11 +4,11 @@
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
  */
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar ('\n');
+		_putchar('\n');
 		return;
 	}
 	_putchar (*s);
