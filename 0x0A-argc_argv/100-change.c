4cbeefedec5331902b805431ab873249625c4b59
kiyuenter <kiyuenterprise@gmail.com>
2023-07-12 05:14:39 -0700

    program that prints the minimum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,46 +6,38 @@
  * @argv: input
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int cents, coins = 0;
-if (argc == 2)
-{
-cents = atoi(*(argv + 1));
-while (cents > 0)
-{
-if (cents % 25 < cents)
-{
-cents -= 25;
-coins++;
-}
-else if (cents % 10 < cents)
-{
-cents -= 10;
-coins++;
-}
-else if (cents % 5 < cents)
-{
-cents -= 5;
-coins++;
-}
-else if (cents % 2 < cents)
-{
-cents -= 2;
-coins++;
-}
-else if (cents % 1 < cents)
-{
-cebts -= 1;
-coins++;
-}
-}
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-printf("%d\n", coins);
-return (0);
+	int coins, i, amount;
+	int value[5] = {25, 10, 5, 2, 1};
+
+	coins = 0;
+	amount = atoi(argv[argc - 1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (amount <= 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (value[i] <= amount)
+			{
+				coins += (amount / value[i]);
+				amount -= (amount / value[i]) * value[i];
+				if (amount == 0)
+				{
+					printf("%d\n", coins);
+					break;
+				}
+			}
+		}
+	}
+	return (0);
 }
