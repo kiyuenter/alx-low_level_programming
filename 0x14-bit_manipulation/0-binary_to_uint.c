81ce7523b78429ad059b264e9420c7f37d406406
kiyuenter <kiyuenterprise@gmail.com>
2023-08-03 06:37:33 -0700

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,29 +2,29 @@
 #include <stdio.h>
 
 /**
- * binary_to_unit - convert binary to unsigned int
+ * binary_to_uint - convert a binary number to an unsigned int
  * @b: char string
- * Return: converted decimal numberor 0 if there is an unconvertable char
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int kidus, power;
-	int zip;
+	unsigned int total, power;
+	int len;
 
 	if (b == NULL)
 		return (0);
 
-	for (zip = 0; b[zip]; zip++)
+	for (len = 0; b[len]; len++)
 	{
-		if (b[zip] != '0' && b[zip] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 	}
 
-	for (power = 1, total = 0, zip--; zip >= 0; zip--, power *= 2)
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
 	{
-		if (b[zip] == '1')
-			kidus += power;
+		if (b[len] == '1')
+			total += power;
 	}
 
-	return (kidus);
+	return (total);
 }
